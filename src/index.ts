2231f87d0a1f720156fdfdfbb1e32aa6969152ce
joaoleandro <new.joaoleandro@gmail.com>
2024-04-18 15:05:43 -0300

    port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from "./server/Server";
 
 server.listen(process.env.PORT, () => {
-    console.log(`rodando na porta ${process.env.PORT || 3333}`)
+    console.log(`rodando na porta ${process.env.PORT || 3000}`)
 })
